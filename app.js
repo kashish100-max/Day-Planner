5e615a1af4b9c608720f7d74b6c001f4e277308b
kashish100-max <kashishatri817@gmail.com>
2025-06-17 00:50:40 +0530

    new changes in sliding

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ let page2=document.querySelector(".main_side_signup");
 btn1.addEventListener("click",()=>{
     page1.classList.add("move");
     page2.classList.remove("move2");
+    page1.style.zIndex = "-1";
+    page2.style.zIndex = "1";
 });
 
 btn2.addEventListener("click",()=>{
     page2.classList.add("move2");
     page1.classList.remove("move");
+    setTimeout(()=>{
+        page1.classList.remove("start");
+    },1000);
+    page2.style.zIndex = "-1";
+    page1.style.zIndex = "1";
 });
