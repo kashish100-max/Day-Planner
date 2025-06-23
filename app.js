5eb8e6048c396e8f0e93207e053dcbafecad8a51
kashish100-max <kashishatri817@gmail.com>
2025-06-23 18:08:25 +0530

    made silders done

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-let btn1=document.querySelector(".move_btn_login");
-let page1=document.querySelector(".main_side_signin");
-let btn2=document.querySelector(".move_btn_signup");
-let page2=document.querySelector(".main_side_signup");
+let btn1 = document.querySelector(".move_btn_login");
+let page1 = document.querySelector(".main_side_signin");
+let btn2 = document.querySelector(".move_btn_signup");
+let page2 = document.querySelector(".main_side_signup");
+let page3=document.querySelector(".login");
+let page4=document.querySelector(".signUp");
 
-btn1.addEventListener("click",()=>{
-    page1.classList.add("move");
-    page2.classList.remove("move2");
-    page1.style.zIndex = "-1";
-    page2.style.zIndex = "1";
+btn1.addEventListener("click", () => {
+  page1.classList.add("move");
+  page2.classList.remove("move2");
+
+  page1.classList.remove("active");
+  page1.classList.add("start");
+
+  page2.classList.remove("start");
+  page2.classList.add("active");
+
+  page4.classList.add("moveLeftPage");
+  page3.classList.remove("moveRightPage");
 });
 
-btn2.addEventListener("click",()=>{
-    page2.classList.add("move2");
-    page1.classList.remove("move");
-    setTimeout(()=>{
-        page1.classList.remove("start");
-    },1000);
-    page2.style.zIndex = "-1";
-    page1.style.zIndex = "1";
+btn2.addEventListener("click", () => {
+  page2.classList.add("move2");
+  page1.classList.remove("move");
+
+  page2.classList.remove("active");
+  page2.classList.add("start");
+
+  page1.classList.remove("start");
+  page1.classList.add("active");
+
+  page4.classList.remove("moveLeftPage");
+  page3.classList.add("moveRightPage");
 });
